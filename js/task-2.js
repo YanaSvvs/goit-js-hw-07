463be8c82a08ce380a409b7df99ec3a858ca25da
Yasmin <jana.afonina@gmail.com>
2025-05-13 20:47:00 +0300

    add task-2

--- js/task-2.js
+++ js/task-2.js
@@ -22,5 +22,22 @@ const images = [
   {
     url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
     alt: "Lighthouse Coast Sea",
-  }
+  },
 ];
+
+const gallery = document.querySelector('.gallery');
+const galleryItems = [];
+
+images.forEach(image => {
+  const listItem = document.createElement('li');
+  listItem.classList.add('gallery-item');
+
+  const img = document.createElement('img');
+  img.src = image.url;
+  img.alt = image.alt;
+
+  listItem.append(img);
+  galleryItems.push(listItem);
+});
+
+gallery.append(...galleryItems);
